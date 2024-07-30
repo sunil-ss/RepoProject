d516f55fb4663e7c4bc54825f9ed28511fb95c9c
sunil-ss <75974856+sunil-ss@users.noreply.github.com>
2024-07-30 21:57:40 +0530

    Update Login.java

--- src/test/java/stepDef/Login.java
+++ src/test/java/stepDef/Login.java
@@ -24,6 +24,7 @@ public class Login {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.out.println("Sunil updated in Github")
 	}
 	
 	@Given("^I login into omayo website$")
